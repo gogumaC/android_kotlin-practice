ea3468f76afd0fedc56df83c3f7974c01020a223
YUBINKIM <kimu2371@naver.com>
2020-10-13 18:40:49 +0900

    20.10.13

---
+++ android_kotlinpractice/app/src/main/java/yubinkim/android_note/android_kotlinpractice/Change1Activity.kt
@@ -0,0 +1,13 @@
+package yubinkim.android_note.android_kotlinpractice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Change1Activity :AppCompatActivity()
+{
+    override fun onCreate(saveInstanceState: Bundle?)
+    {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_change1)
+    }
+}
